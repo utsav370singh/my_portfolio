ec17a0d26860594927a7b4d580a1da7041e9c78b
utsav370singh <130243600+utsav370singh@users.noreply.github.com>
2024-10-07 20:21:00 +0530

    photo

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -33,7 +33,7 @@ const About = () => {
                     </div>
                 </div>
                 <div className="hidden lg:flex lg:w-5/12 w-full justify-center md:justify-end items-center">
-                    <img src="/images/utsav1.png" alt="Utsav Singh" className="w-full max-w-xs h-auto rounded-lg" />
+                    <img src="" alt="Utsav Singh" className="w-full max-w-xs h-auto rounded-lg" />
                 </div>
             </div>
         </div>
