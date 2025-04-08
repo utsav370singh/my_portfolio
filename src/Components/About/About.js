cb28456818f3ec0d4974ad03a76e7f0073c4e0f4
Utsav Singh <130243600+utsav370singh@users.noreply.github.com>
2025-04-08 18:13:28 +0530

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -34,7 +34,6 @@ const About = () => {
                 </div>
                 <div className="hidden lg:flex lg:w-5/12 w-full justify-center md:justify-end items-center">
                     <img src="/images/utsav.png" alt="Utsav Singh" className="w-full max-w-lg h-auto rounded-lg" />
-                    <img src="utsav.jpg" alt="Utsav Singh" className="w-full max-w-xs h-auto rounded-lg" />
                 </div>
             </div>
         </div>
